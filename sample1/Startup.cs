af7a7f1749e8ff428c83c67bc5f221ffc5faf518
アレックス <alexjapan2021@outlook.com>
2017-03-26 22:10:25 +0900

    initial commit

---
+++ sample1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sample1.Startup))]
+namespace sample1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
